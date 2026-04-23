de8f8bf3afff559fc10d70ef16d597390aeea048
corcusstudio258 <corcusweb@gmail.com>
2026-04-23 10:21:51 +0000

    form

--- api/leads.js
+++ api/leads.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const GOOGLE_SCRIPT_URL =
   process.env.GOOGLE_SCRIPT_URL ||
-  "https://script.google.com/macros/s/AKfycbyW9-x4wX6dS5R9xzmr2sIreQWRyxWgi4CYuRVDVnUAo7xjQXK2lZbV0zr8HuAFIChI/exec";
+  "https://script.google.com/macros/s/AKfycbwfbaeYaS3OcU4P6Uy8-RoOUl-4Os8LMJItHM778quPrLBOR8gGThy-wao8ZmWD0T1X/exec";
 const GOOGLE_SHEET_ID =
   process.env.GOOGLE_SHEET_ID ||
   "1KrS1vB9EzYV21NeOJf6z6Ls71adx509R_3Mnm2cdI_k";
--- src/services/googleSheetService.js
+++ src/services/googleSheetService.js
@@ -1,6 +1,6 @@
 export const GOOGLE_SHEET_ID = "1KrS1vB9EzYV21NeOJf6z6Ls71adx509R_3Mnm2cdI_k";
 export const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbyW9-x4wX6dS5R9xzmr2sIreQWRyxWgi4CYuRVDVnUAo7xjQXK2lZbV0zr8HuAFIChI/exec";
+  "https://script.google.com/macros/s/AKfycbwfbaeYaS3OcU4P6Uy8-RoOUl-4Os8LMJItHM778quPrLBOR8gGThy-wao8ZmWD0T1X/exec";
 export const GOOGLE_RECIPIENT_EMAIL = "corcusweb@gmail.com";
 export const GOOGLE_SUBJECT = "New contact form submission from Corcus website";
 export const GOOGLE_SHEET_URL =
